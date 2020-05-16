62f0aed28d850fd5d25f319cad082afda444f70b
ValKiriann <insanityanni@gmail.com>
2020-05-16 17:28:00 +0200

    creating router

--- server.js
+++ server.js
@@ -1,31 +1,11 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
-const router = express.Router();
-const PORT = 3000;
-const PACKAGE = require("./package.json");
-
-const responseUtils = require('./utils/response.utils');
+const {port} = require('./config.json');
+const router = require('./routes/network');
 
 let app = express();
 app.use(bodyParser.json({ extended: false }));
 app.use(router);
+app.listen(port);
 
-router.get('/', function(req,res){ 
-    responseUtils.success(req,res,200,`Module ${PACKAGE.name} is up at ${PORT}`);
-});
-
-router.get('/ping', function(req,res){ 
-    responseUtils.success(req,res,200,'Pong');
-});
-
-router.post('/test/body', function(req,res){ 
-    responseUtils.success(req,res,req.body);
-});
-
-router.get('/test/query', function(req,res){ 
-    responseUtils.success(req,res,req.query);
-});
-
-app.listen(PORT);
-
-console.log(`Listening to http://localhost:${PORT}`);
+console.log(`Listening to http://localhost:${port}`);
